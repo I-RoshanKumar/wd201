f998d75d16ddf7d58fccba756e66bd23acda230d
I-RoshanKumar <anitamishraasansol@gmail.com>
2024-09-21 23:19:49 +0530

    Adding level2 project

---
+++ http-server/index.js
@@ -0,0 +1,59 @@
+const http = require("http");
+const fs = require("fs");
+const minimist = require('minimist');
+const args = minimist(process.argv.slice(1), {
+  default: {
+    PORT: 3000
+  }
+});
+
+
+
+let homeContent = "";
+let projectContent = "";
+let registrationcontent="";
+
+const num1 = parseInt(args.num1);
+
+
+fs.readFile("home.html", (err, home) => {
+  if (err) {
+    throw err;
+  }
+  homeContent = home;
+});
+
+fs.readFile("project.html", (err, project) => {
+  if (err) {
+    throw err;
+  }
+  projectContent = project;
+});
+
+fs.readFile("registration.html",(err,registration)=>{
+  if(err){
+    throw err;
+  }
+  registrationcontent=registration;
+})
+http
+  .createServer((request, response) => {
+    let url = request.url;
+    response.writeHeader(200, { "Content-Type": "text/html" });
+    switch (url) {
+      case "/project":
+        response.write(projectContent);
+        response.end();
+        break;
+      case "/registration":
+        response.write(registrationcontent);
+        response.end();
+        break;
+      default:
+        response.write(homeContent);
+        response.end();
+        break;
+    }
+  })
+  .listen(args.PORT);
+  console.log(`Server running on: ${args.PORT}`);
