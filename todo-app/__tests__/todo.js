26504a6459f727593d6a6f22242d57cb759d81ac
I-RoshanKumar <anitamishraasansol@gmail.com>
2024-10-05 19:12:08 +0530

    adding files

---
+++ todo-app/__tests__/todo.js
@@ -0,0 +1,33 @@
+/* eslint-disable no-undef */
+const request=require("supertest");
+const db=require("../models/index");
+const app=require("../app");    
+
+
+let server,agent;
+
+describe("Todo test suite", () => {
+    beforeAll(async () => {
+        await db.sequelize.sync({ force: true });
+        server=app.listen(3000, () => {
+            console.log("Started express server at port 3000");
+        });
+        agent=request.agent(server);
+    });
+    afterAll(async ()=> {
+        await db.sequelize.close();
+        await server.close();
+    });
+    test('responds with json at /todos',async () => {
+      const response=await agent.post("/todos").send({
+        'title': "Buy milk",
+        dueDate: new Date().toISOString(),
+        completed: false,
+      });
+      expect(response.statusCode).toBe(200);
+      expect(response.header["content-type"]).toBe("application/json; charset=utf-8");
+      const parsedResponse = JSON.parse(response.text);
+      expect(parsedResponse.id).toBeDefined();
+    });
+    
+});
--- todo-app/index.js
+++ todo-app/index.js
@@ -1,8 +1,5 @@
-const express = require('express');
-const app = express();
-app.get('/', function (request, response) {
-    response.send('Hello World')
-  });
-  app.listen(3000, () => {
-    console.log("Server listened at port 3000");
-  });
+const app = require("./app");
+
+app.listen(3000, () => {
+  console.log("Started express server at port 3000");
+});
